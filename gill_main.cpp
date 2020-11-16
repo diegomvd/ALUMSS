5c4273bddfba6e2d21b4818fabbbc7fe7200cbd6
diego <bengo.bengo@protonmail.com>
2020-11-16 12:28:53 +0100

    troubleshooting in euk

--- gill_main.cpp
+++ gill_main.cpp
@@ -386,6 +386,17 @@ int main(int argc, const char * argv[]){
   auto stop = chrono::high_resolution_clock::now();
   auto duration = chrono::duration_cast<chrono::minutes>(stop - start);
   cout << "total execution time " << duration.count() << endl;
-  cout << "Time out of simulation =" << t <<", last dtg= " << dtg << ", Cumsum= " << propensityVector.back() <<  "\n"; 
+  cout << "Time out of simulation =" << t <<", last dtg= " << dtg << ", Cumsum= " << propensityVector.back() <<  "\n";
+  cout << "looking for nan emplacement... \n";
+  for(ix=0;ix<propensityVector.size();ix++){
+    if isnan(propensityVector[ix]){
+      i=ix;
+      break;
+    }
+  }
+  reaction=(int)i/(n*n); //result from euclidian division
+  patch=i%(n*n); // remainder from euclidian division
+  cout << "Error in reaction "<< reaction << " at patch " << patch <<"\n";
+
   return 0;
 }
