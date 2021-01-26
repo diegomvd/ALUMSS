9b467a63fa770027cec337bfcfa0c343adfe4b15
diego <bengo.bengo@protonmail.com>
2021-01-26 15:28:13 +0100

    new run to eukaryote lower tag

--- runsim.sh
+++ runsim.sh
@@ -12,11 +12,20 @@ COMMENT1
 
 #run
 
-./gillespie-ses 4000 0.1 40 0.2 0 1 0.3 0.0 0 0.5 50 2 50 1 1 testa&
-./gillespie-ses 4000 0.1 40 0.2 0 1 0.3 0.1 0 0.5 50 2 50 1 1 testa&
-./gillespie-ses 4000 0.1 40 0.2 0 1 0.3 0.2 0 0.5 50 2 50 1 1 testa&
-./gillespie-ses 4000 0.1 40 0.2 0 1 0.3 0.4 0 0.5 50 2 50 1 1 testa&
-./gillespie-ses 4000 0.1 40 0.2 0 1 0.3 0.8 0 0.5 50 2 50 1 1 testa&
-./gillespie-ses 4000 0.1 40 0.2 0 1 0.3 1.6 0 0.5 50 2 50 1 1 testa&
-./gillespie-ses 4000 0.1 40 0.2 0 1 0.3 3.2 0 0.5 50 2 50 1 1 testa&
-./gillespie-ses 4000 0.1 40 0.2 0 1 0.3 6.4 0 0.5 50 2 50 1 1 testa&
+./gillespie-ses 4000 0.1 40 0.2 0 1 0.3 0.0 0 0.1 50 2 50 1 1 testa&
+./gillespie-ses 4000 0.1 40 0.2 0 1 0.3 0.1 0 0.1 50 2 50 1 1 testa&
+./gillespie-ses 4000 0.1 40 0.2 0 1 0.3 0.2 0 0.1 50 2 50 1 1 testa&
+./gillespie-ses 4000 0.1 40 0.2 0 1 0.3 0.4 0 0.1 50 2 50 1 1 testa&
+./gillespie-ses 4000 0.1 40 0.2 0 1 0.3 0.8 0 0.1 50 2 50 1 1 testa&
+./gillespie-ses 4000 0.1 40 0.2 0 1 0.3 1.6 0 0.1 50 2 50 1 1 testa&
+./gillespie-ses 4000 0.1 40 0.2 0 1 0.3 3.2 0 0.1 50 2 50 1 1 testa&
+./gillespie-ses 4000 0.1 40 0.2 0 1 0.3 6.4 0 0.1 50 2 50 1 1 testa&
+
+./gillespie-ses 4000 0.1 40 0.2 0 1 0.3 0.0 0 0.05 50 2 50 1 1 testb&
+./gillespie-ses 4000 0.1 40 0.2 0 1 0.3 0.1 0 0.05 50 2 50 1 1 testb&
+./gillespie-ses 4000 0.1 40 0.2 0 1 0.3 0.2 0 0.05 50 2 50 1 1 testb&
+./gillespie-ses 4000 0.1 40 0.2 0 1 0.3 0.4 0 0.05 50 2 50 1 1 testb&
+./gillespie-ses 4000 0.1 40 0.2 0 1 0.3 0.8 0 0.05 50 2 50 1 1 testb&
+./gillespie-ses 4000 0.1 40 0.2 0 1 0.3 1.6 0 0.05 50 2 50 1 1 testb&
+./gillespie-ses 4000 0.1 40 0.2 0 1 0.3 3.2 0 0.05 50 2 50 1 1 testb&
+./gillespie-ses 4000 0.1 40 0.2 0 1 0.3 6.4 0 0.05 50 2 50 1 1 testb&
